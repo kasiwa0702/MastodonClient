b396f79ff8f35b2b9a308fd774f21cbce1c2c49f
kasiwa0702 <yk20010702@gmail.com>
2021-06-27 15:56:11 +0900

    no2

--- app/src/main/java/io/keiji/sample/mastodonclient/MainActivity.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/MainActivity.kt
@@ -2,10 +2,14 @@ package io.keiji.sample.mastodonclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textview)
+        textView.text = "Hello XML Layout"
     }
 }
