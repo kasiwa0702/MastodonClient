8c400d8d8988df9cb8e3518cb513c8f709d64455
kasiwa0702 <yk20010702@gmail.com>
2021-07-03 15:55:23 +0900

    no4 sttp5

--- app/src/main/java/io/keiji/sample/mastodonclient/MainActivity.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/MainActivity.kt
@@ -1,17 +1,24 @@
 package io.keiji.sample.mastodonclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
-            this,
-            R.layout.activity_main
-        )
-        binding.textview.text = "Hello DataBinding"
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            val fragment = MainFragment()
+            supportFragmentManager.beginTransaction()
+                .add(
+                    R.id.fragment_container,
+                    fragment,
+                    MainFragment::class.java.simpleName
+                )
+                .commit()
+        }
     }
 }
