2da98b35573ba6794ed47b86fa143ce1539758a9
kasiwa0702 <yk20010702@gmail.com>
2021-08-14 22:18:14 +0900

    no7 step1

--- app/src/main/java/io/keiji/sample/mastodonclient/TootListFragment.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/TootListFragment.kt
@@ -18,7 +18,6 @@ class TootListFragment : Fragment(R.layout.fragment_toot_list) {
 
     companion object{
         val TAG = TootListFragment::class.java.simpleName
-        private const val API_BASE_URL = "https://androidbook2020.keiji.io"
     }
     private var binding: FragmentTootListBinding? = null
 
@@ -27,7 +26,7 @@ class TootListFragment : Fragment(R.layout.fragment_toot_list) {
 
    private val viewModel: TootListViewModel by viewModels {
        TootListViewModelFactory(
-           API_BASE_URL,
+           BuildConfig.INSTANCE_URL,
            lifecycleScope,
            requireContext()
        )
@@ -98,7 +97,3 @@ class TootListFragment : Fragment(R.layout.fragment_toot_list) {
         binding?.unbind()
     }
 }
-
-
-
-
