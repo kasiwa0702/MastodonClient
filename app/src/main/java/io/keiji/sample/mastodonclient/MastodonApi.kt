0bbb70b4a13cc7555c9133fe380b389a5d55fc9e
kasiwa0702 <yk20010702@gmail.com>
2021-08-19 23:51:59 +0900

    no7 step2

--- app/src/main/java/io/keiji/sample/mastodonclient/MastodonApi.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/MastodonApi.kt
@@ -2,6 +2,7 @@ package io.keiji.sample.mastodonclient
 
 
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface MastodonApi  {
@@ -11,4 +12,9 @@ interface MastodonApi  {
         @Query("max_id") maxId: String? = null,
         @Query("only_media") onlyMedia: Boolean = false
     ): List<Toot>
+    @GET("api/vl/timelines/home")
+    suspend fun fetchHomeTimeline(
+        @Header("Authorization") accessToken: String,
+        @Query("max_id") maxId: String? = null
+    ) : List<Toot>
 }
