9bef979a8b6758a34b85e9c50608b76c148be00a
kasiwa0702 <yk20010702@gmail.com>
2021-09-09 00:31:49 +0900

    no9 step0.6

---
+++ app/src/main/java/io/keiji/sample/mastodonclient/ui/toot_edit/TootEditFragment.kt
@@ -0,0 +1,14 @@
+package io.keiji.sample.mastodonclient.ui.toot_edit
+
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import io.keiji.sample.mastodonclient.BuildConfig
+import io.keiji.sample.mastodonclient.R
+import io.keiji.sample.mastodonclient.databinding.FragmentTootEditBinding
+
+class TootEditFragment : Fragment(R.layout.fragment_toot_edit) {
+}
