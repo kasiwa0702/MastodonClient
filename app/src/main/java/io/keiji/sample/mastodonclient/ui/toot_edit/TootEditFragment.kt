4cdba62cfe72959d3af5f993f7fbe9577ab0a2b4
kasiwa0702 <yk20010702@gmail.com>
2021-09-10 01:06:11 +0900

    no9 step1

--- app/src/main/java/io/keiji/sample/mastodonclient/ui/toot_edit/TootEditFragment.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/ui/toot_edit/TootEditFragment.kt
@@ -11,4 +11,39 @@ import io.keiji.sample.mastodonclient.R
 import io.keiji.sample.mastodonclient.databinding.FragmentTootEditBinding
 
 class TootEditFragment : Fragment(R.layout.fragment_toot_edit) {
+
+    companion object{
+        val TAG = TootEditFragment::class.java.simpleName
+
+        fun newInstance(): TootEditFragment{
+            return TootEditFragment()
+        }
+    }
+
+    private var binding: FragmentTootEditBinding? = null
+
+    private val viewModel: TootEditViewModel by viewModels {
+        TootEditViewModelFactory(
+            BuildConfig.INSTANCE_URL,
+            BuildConfig.USERNAME,
+            lifecycleScope,
+            requireContext()
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bindingData: FragmentTootEditBinding? = DataBindingUtil.bind(view)
+        binding = bindingData ?: return
+
+        bindingData.lifecycleOwner = viewLifecycleOwner
+        bindingData.viewModel = viewModel
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        binding?.unbind()
+    }
 }
