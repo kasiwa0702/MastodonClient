87908704e4a172a104903b4ab9bc140a8d99c0c0
kasiwa0702 <yk20010702@gmail.com>
2021-09-24 02:51:48 +0900

    No10 step3.5

--- app/src/main/java/io/keiji/sample/mastodonclient/ui/login/LoginFragment.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/ui/login/LoginFragment.kt
@@ -1,7 +1,10 @@
 package io.keiji.sample.mastodonclient.ui.login
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -9,6 +12,7 @@ import androidx.lifecycle.lifecycleScope
 import io.keiji.sample.mastodonclient.BuildConfig
 import io.keiji.sample.mastodonclient.R
 import io.keiji.sample.mastodonclient.databinding.FragmentLoginBinding
+import kotlin.reflect.jvm.internal.impl.resolve.scopes.InnerClassesScopeWrapper
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
@@ -26,10 +30,43 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         )
     }
 
+    private val onObtainCode = fun (code: String) {
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val bindingData: FragmentLoginBinding? = DataBindingUtil.bind(view)
         binding = bindingData ?: return
+
+        val authUri = Uri.parse(BuildConfig.INSTANCE_URL)
+            .buildUpon()
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id",BuildConfig.CLIENT_KEY)
+            .appendQueryParameter("redirect",BuildConfig.CLIENT_REDIRECT_URI)
+            .appendQueryParameter("response_type","code")
+            .appendQueryParameter("scope",BuildConfig.CLIENT_SCOPES)
+            .build()
+
+        bindingData.webview.webViewClient = InnerWebViewClient(onObtainCode)
+        bindingData.webview.settings.javaScriptEnabled = true
+        bindingData.webview.loadUrl(authUri.toString())
+
+    }
+
+    private class InnerWebViewClient(
+        val onObtainCode: (code: String) -> Unit
+    ) : WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            view ?: return
+
+            val code = Uri.parse(view.url).getQueryParameter("code")
+            code ?: return
+
+            onObtainCode(code)
+        }
     }
 }
